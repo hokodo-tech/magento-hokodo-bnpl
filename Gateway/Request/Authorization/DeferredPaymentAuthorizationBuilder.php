a200612a1eefe75713a87aa52b0e1b9919582815
lykhachov <>
2022-05-25 20:02:43 +0300

    Added scope support for actions with order from the admin. Added extra Authorization builders for retrieving correct API key

---
+++ Gateway/Request/Authorization/DeferredPaymentAuthorizationBuilder.php
@@ -0,0 +1,51 @@
+<?php
+declare(strict_types=1);
+
+namespace Hokodo\BNPL\Gateway\Request\Authorization;
+
+use Hokodo\BNPL\Gateway\Config\Config;
+use Magento\Framework\Api\SearchCriteriaBuilderFactory;
+use Magento\Sales\Api\OrderRepositoryInterface;
+
+class DeferredPaymentAuthorizationBuilder extends \Hokodo\BNPL\Gateway\Request\AuthorizationBuilder
+{
+    /**
+     * @var OrderRepositoryInterface
+     */
+    private $orderRepository;
+
+    /**
+     * @var SearchCriteriaBuilderFactory
+     */
+    private $criteriaBuilderFactory;
+
+    public function __construct(
+        Config $config,
+        OrderRepositoryInterface $orderRepository,
+        SearchCriteriaBuilderFactory $criteriaBuilderFactory
+    ) {
+        parent::__construct(
+            $config
+        );
+        $this->orderRepository = $orderRepository;
+        $this->criteriaBuilderFactory = $criteriaBuilderFactory;
+    }
+
+    /**
+     * @param array $buildSubject
+     * @return array[]
+     * @throws \Magento\Framework\Exception\LocalizedException
+     */
+    public function build(array $buildSubject): array
+    {
+        $storeId = null;
+        if (isset($buildSubject['payment'])) {
+            $storeId = (int) $buildSubject['payment']->getOrder()->getStoreId();
+        }
+        return [
+            'header' => [
+                'Authorization' => sprintf('Token %s', $this->getAuthorizationKey($storeId)),
+            ],
+        ];
+    }
+}
