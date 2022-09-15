5dc3ce1b7c5c86d8164fef95d3848ae099f3880f
lykhachov <>
2022-09-15 18:25:30 +0300

    Added Pathc order call to update unique_order_is with magento real ID

---
+++ Model/Data/Gateway/PatchOrderRequest.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Copyright © 2018-2021 Hokodo. All Rights Reserved.
+ * See LICENSE for license details.
+ */
+declare(strict_types=1);
+
+namespace Hokodo\BNPL\Model\Data\Gateway;
+
+class PatchOrderRequest
+{
+
+}
