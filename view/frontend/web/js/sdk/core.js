0a82db48834e28e37e39e0d1914d1298638b4782
martyMcFly85 <>
2022-10-04 16:34:49 +0300

    init SDK only when user is logged in

--- view/frontend/web/js/sdk/core.js
+++ view/frontend/web/js/sdk/core.js
@@ -1,14 +1,19 @@
-define(['jquery'], function ($) {
+define([
+    'jquery',
+    'Hokodo_BNPL/js/sdk/customer'
+], function ($, customer) {
     return function (config) {
-        require([config.url], function () {
-            if (!window.hokodoSdk) {
-                let sdkConfig = {};
-                if (config.faq) {
-                    sdkConfig.faqLink = config.faq
-                }
-                window.hokodoSdk = Hokodo(config.key, sdkConfig);
-                console.log('hokodo Core loaded');
-                $('body').triggerHandler('hokodoSdkResolved');
+        customer().done((name) => {
+            if (name !== undefined && !window.hokodoSdk) {
+                require([config.url], function () {
+                    let sdkConfig = {};
+                    if (config.faq) {
+                        sdkConfig.faqLink = config.faq
+                    }
+                    window.hokodoSdk = Hokodo(config.key, sdkConfig);
+                    console.log('hokodo Core loaded');
+                    $('body').triggerHandler('hokodoSdkResolved');
+                })
             }
         })
     }
