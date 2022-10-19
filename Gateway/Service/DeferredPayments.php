aa4087b7bc6cc75aa9bbbd5d47872ecdb2787b9f
martyMcFly85 <>
2022-10-19 17:20:39 +0300

    added actions to DF service

--- Gateway/Service/DeferredPayments.php
+++ Gateway/Service/DeferredPayments.php
@@ -23,4 +23,44 @@ class DeferredPayments extends AbstractService
         $postSaleAction->setType(DeferredPaymentsPostSaleActionInterface::TYPE_CAPTURE);
         return $this->commandPool->get('hokodo_post_sale')->execute($postSaleAction->__toArray());
     }
+
+    /**
+     * @throws NotFoundException
+     * @throws CommandException
+     */
+    public function captureRemaining(DeferredPaymentsPostSaleActionInterface $postSaleAction): ResultInterface
+    {
+        $postSaleAction->setType(DeferredPaymentsPostSaleActionInterface::TYPE_CAPTURE_REMAINING);
+        return $this->commandPool->get('hokodo_post_sale')->execute($postSaleAction->__toArray());
+    }
+
+    /**
+     * @throws NotFoundException
+     * @throws CommandException
+     */
+    public function refund(DeferredPaymentsPostSaleActionInterface $postSaleAction): ResultInterface
+    {
+        $postSaleAction->setType(DeferredPaymentsPostSaleActionInterface::TYPE_REFUND);
+        return $this->commandPool->get('hokodo_post_sale')->execute($postSaleAction->__toArray());
+    }
+
+    /**
+     * @throws NotFoundException
+     * @throws CommandException
+     */
+    public function void(DeferredPaymentsPostSaleActionInterface $postSaleAction): ResultInterface
+    {
+        $postSaleAction->setType(DeferredPaymentsPostSaleActionInterface::TYPE_VOID);
+        return $this->commandPool->get('hokodo_post_sale')->execute($postSaleAction->__toArray());
+    }
+
+    /**
+     * @throws NotFoundException
+     * @throws CommandException
+     */
+    public function voidRemaining(DeferredPaymentsPostSaleActionInterface $postSaleAction): ResultInterface
+    {
+        $postSaleAction->setType(DeferredPaymentsPostSaleActionInterface::TYPE_VOID_REMAINING);
+        return $this->commandPool->get('hokodo_post_sale')->execute($postSaleAction->__toArray());
+    }
 }
