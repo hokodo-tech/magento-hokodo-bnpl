140df11f361277ca6b148698eefb5689e95efb25
Gavin Clark <gavin.clark@hokodo.co>
2022-10-21 10:03:50 +0100

    Retain traces for more than just retries.  This allows easier debugging of failed tests.

--- tests/playwright/playwright.config.ts
+++ tests/playwright/playwright.config.ts
@@ -42,7 +42,7 @@ const config: PlaywrightTestConfig = {
     baseURL: process.env.BASE_URL,
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: process.env.CI ? "on-first-retry" : "on",
+    trace: process.env.CI ? "retain-on-failure" : "on",
   },
 
   /* Configure projects for major browsers */
