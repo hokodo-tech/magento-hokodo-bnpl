6304da777d1b48cbc88a8bd56f40fba45d75df13
martyMcFly85 <>
2022-10-24 23:13:47 +0300

    adding capture method, refactor http client

---
+++ Exception/ApiGatewayException.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Copyright © 2018-2021 Hokodo. All Rights Reserved.
+ * See LICENSE for license details.
+ */
+declare(strict_types=1);
+
+namespace Hokodo\BNPL\Exception;
+
+use Magento\Framework\Exception\LocalizedException;
+
+class ApiGatewayException extends LocalizedException
+{
+
+}
