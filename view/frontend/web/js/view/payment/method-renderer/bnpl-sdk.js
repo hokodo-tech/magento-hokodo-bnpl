0527ca4116384f830db19606dcf59e9e4af1636d
Ilia Sokolov <ilia.sokolov@hokodo.co>
2022-11-25 17:01:37 +0300

    211 - New verticals (Covento) - Hide company search component if company is already matched

--- view/frontend/web/js/view/payment/method-renderer/bnpl-sdk.js
+++ view/frontend/web/js/view/payment/method-renderer/bnpl-sdk.js
@@ -34,6 +34,7 @@ define([
             searchInitialized: false
         },
         isOfferLoading: ko.observable(false),
+        hasCompanyId: ko.observable(false),
         hokodoElements: window.hokodoSdk.elements(),
         getLogos: ko.observableArray(paymentConfig.logos),
 
@@ -57,6 +58,7 @@ define([
             })
 
             if (this.hokodoCheckout().companyId()) {
+                this.hasCompanyId(true);
                 this.companySearch = this.hokodoElements.create("companySearch", {companyId: this.hokodoCheckout().companyId()});
             } else {
                 this.companySearch = this.hokodoElements.create("companySearch");
