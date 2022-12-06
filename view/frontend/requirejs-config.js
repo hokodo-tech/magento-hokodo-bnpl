5f4c96dc51d3c882be66ed05355ac815524f9720
Doc Brown <ilia.sokolov@hokodo.co>
2022-12-06 20:28:38 +0300

    217-Refactoring - Remove un-used code

--- view/frontend/requirejs-config.js
+++ view/frontend/requirejs-config.js
@@ -9,9 +9,6 @@ config = {
             'Hokodo_BNPL/js/view/payment/method-renderer/bnpl-sdk': {
                 'Hokodo_BNPL/js/segment/method-renderer': true
             },
-            'Magento_Checkout/js/view/minicart': {
-                'Hokodo_BNPL/js/mixins/minicart-mixin': true
-            },
             'Magento_Checkout/js/model/shipping-save-processor': {
                 'Hokodo_BNPL/js/sdk/mixin/shipping-save-processor-mixin': true
             },
