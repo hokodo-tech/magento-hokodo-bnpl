f8341a22baa7f53939fbca0a2e5967f426bb0419
Doc Brown <ilia.sokolov@hokodo.co>
2022-12-12 22:40:22 +0300

    228 - Settings - Add a switch to allow User to change company (or not) on the account page

--- Controller/Customer/Index.php
+++ Controller/Customer/Index.php
@@ -8,11 +8,15 @@ declare(strict_types=1);
 
 namespace Hokodo\BNPL\Controller\Customer;
 
+use Hokodo\BNPL\Gateway\Config\Config;
 use Magento\Customer\Model\Session as CustomerSession;
 use Magento\Framework\App\Action\HttpGetActionInterface;
 use Magento\Framework\App\ObjectManager;
 use Magento\Framework\Controller\ResultFactory;
 use Magento\Framework\Controller\ResultInterface;
+use Magento\Framework\Exception\NoSuchEntityException;
+use Magento\Framework\UrlInterface;
+use Magento\Store\Model\StoreManagerInterface;
 
 class Index implements HttpGetActionInterface
 {
@@ -27,14 +31,23 @@ class Index implements HttpGetActionInterface
     private ResultFactory $resultFactory;
 
     /**
-     * @param ResultFactory        $resultFactory
-     * @param CustomerSession|null $customerSession
+     * @param ResultFactory         $resultFactory
+     * @param UrlInterface          $urlBuilder
+     * @param StoreManagerInterface $storeManager
+     * @param Config                $config
+     * @param CustomerSession|null  $customerSession
      */
     public function __construct(
         ResultFactory $resultFactory,
+        UrlInterface $urlBuilder,
+        StoreManagerInterface $storeManager,
+        Config $config,
         CustomerSession $customerSession = null
     ) {
         $this->resultFactory = $resultFactory;
+        $this->urlBuilder = $urlBuilder;
+        $this->storeManager = $storeManager;
+        $this->config = $config;
         $this->customerSession = $customerSession ?: ObjectManager::getInstance()->get(CustomerSession::class);
     }
 
@@ -49,8 +62,32 @@ class Index implements HttpGetActionInterface
             return $this->resultFactory->create(ResultFactory::TYPE_REDIRECT)
                 ->setPath('customer/account/login');
         }
+
+        try {
+            $currentStore = $this->storeManager->getStore();
+        } catch (NoSuchEntityException $e) {
+            return $this->getCustomerAccountRedirect();
+        }
+
+        if (!$this->config->isAllowCustomerToChangeCompany((int) $currentStore->getId())) {
+            return $this->getCustomerAccountRedirect();
+        }
+
         $resultPage = $this->resultFactory->create(ResultFactory::TYPE_PAGE);
         $resultPage->getConfig()->getTitle()->set(__('My Company'));
         return $resultPage;
+    }
+
+    /**
+     * Get Redirect to the Customer Account Page.
+     *
+     * @return ResultInterface
+     */
+    public function getCustomerAccountRedirect(): ResultInterface
+    {
+        $url = $this->urlBuilder->getUrl('customer/account', ['_secure' => true]);
+        $resultRedirect = $this->resultFactory->create(ResultFactory::TYPE_REDIRECT);
+        $resultRedirect->setUrl($url);
+        return $resultRedirect;
     }
 }
