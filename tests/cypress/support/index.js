cbc251ea89f4cc75567ee37dd89402b3eac682b4
Gavin Clark <gavin.clark@hokodo.co>
2022-12-13 15:13:08 +0000

    Remove Cypress from the project. We don't use it any more

--- tests/cypress/support/index.js
+++
@@ -1,7 +0,0 @@
-import '../support/commands'
-
-Cypress.on("uncaught:exception", (err, runnable) => {
-    // this is purely to ignore errors from recaptcha
-    // TODO: replace with something more explicit
-    return false
-});
