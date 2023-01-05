4a704bddcf06496a0bff1f87ead1fb86158316b0
martyMcFly85 <>
2023-01-05 17:52:33 +0200

    Changed config path for entity level

---
+++ Model/Config/Source/EntityLevelForSave.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Copyright © 2018-2021 Hokodo. All Rights Reserved.
+ * See LICENSE for license details.
+ */
+
+declare(strict_types=1);
+
+namespace Hokodo\BNPL\Model\Config\Source;
+
+use Magento\Framework\Data\OptionSourceInterface;
+
+class EntityLevelForSave
+{
+    public const CUSTOMER = 'customer';
+}
