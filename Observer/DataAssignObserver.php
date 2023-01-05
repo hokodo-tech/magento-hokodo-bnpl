9192eb649d954534a4df0adfba30eb11c6fedca9
Doc Brown <ilia.sokolov@hokodo.co>
2023-01-05 21:48:11 +0400

    235 - Implementation of Payment Terms

--- Observer/DataAssignObserver.php
+++ Observer/DataAssignObserver.php
@@ -8,6 +8,7 @@ namespace Hokodo\BNPL\Observer;
 
 use Hokodo\BNPL\Gateway\Service\Offer as OfferGatewayService;
 use Hokodo\BNPL\Gateway\Service\Order;
+use Hokodo\BNPL\Service\PaymentTerms;
 use Magento\Framework\Event\Observer;
 use Magento\Payment\Observer\AbstractDataAssignObserver;
 use Magento\Quote\Api\Data\PaymentInterface;
@@ -22,6 +23,9 @@ class DataAssignObserver extends AbstractDataAssignObserver
     public const HOKODO_ORDER_ID = 'hokodo_order_id';
     public const HOKODO_PAYMENT_OFFER_ID = 'hokodo_payment_offer_id';
     public const HOKODO_DEFERRED_PAYMENT_ID = 'hokodo_deferred_payment_id';
+    public const HOKODO_PAYMENT_PLAN_NAME = 'name';
+    public const HOKODO_PAYMENT_TERMS_RELATIVE_TO = 'payment_terms_relative_to';
+    public const HOKODO_PAYMENT_TERMS = 'payment_terms';
     public const HOKODO_PAYMENT_PLAN_DUE_DATE = 'due_date';
     public const HOKODO_PAYMENT_METHOD = 'payment_method';
 
@@ -47,14 +51,22 @@ class DataAssignObserver extends AbstractDataAssignObserver
     private OfferGatewayService $offerGatewayService;
 
     /**
+     * @var PaymentTerms
+     */
+    private PaymentTerms $paymentTerms;
+
+    /**
      * @param Order               $orderService
+     * @param PaymentTerms        $paymentTerms
      * @param OfferGatewayService $offerGatewayService
      */
     public function __construct(
         Order $orderService,
+        PaymentTerms $paymentTerms,
         OfferGatewayService $offerGatewayService
     ) {
         $this->orderService = $orderService;
+        $this->paymentTerms = $paymentTerms;
         $this->offerGatewayService = $offerGatewayService;
     }
 
@@ -95,6 +107,11 @@ class DataAssignObserver extends AbstractDataAssignObserver
                     if ($paymentPlan->getStatus() != \Hokodo\BNPL\Api\Data\DeferredPaymentInterface::STATUS_ACCEPTED) {
                         continue;
                     }
+                    $additionalData[self::HOKODO_PAYMENT_PLAN_NAME] = $paymentPlan->getName();
+                    $additionalData[self::HOKODO_PAYMENT_TERMS_RELATIVE_TO] = $paymentPlan->getPaymentTermsRelativeTo();
+                    $additionalData[self::HOKODO_PAYMENT_TERMS] = $this->paymentTerms
+                        ->getPaymentTerms($paymentPlan->getName(), $paymentPlan->getPaymentTermsRelativeTo());
+
                     $scheduledPayments = $paymentPlan->getScheduledPayments();
                     foreach ($scheduledPayments as $scheduledPayment) {
                         list($year, $month, $day) = explode('-', $scheduledPayment->getDate());
---
+++ Service/PaymentTerms.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Copyright © 2018-2021 Hokodo. All Rights Reserved.
+ * See LICENSE for license details.
+ */
+
+declare(strict_types=1);
+
+namespace Hokodo\BNPL\Service;
+
+class PaymentTerms
+{
+    /**
+     * Try to get Payment Terms.
+     *
+     * @param string $name
+     * @param string $paymentTermsRelativeTo
+     *
+     * @return \Magento\Framework\Phrase|null
+     */
+    public function getPaymentTerms(string $name, string $paymentTermsRelativeTo): ?\Magento\Framework\Phrase
+    {
+        $afterDelivery = false;
+        $endOfMonth = false;
+        $name = trim(strtolower($name));
+        $paymentTermsRelativeTo = trim(strtolower($paymentTermsRelativeTo));
+
+        if (!str_contains($name, 'd')) {
+            return null;
+        }
+        $numberOfDays = (int) $name;
+
+        if ($paymentTermsRelativeTo == 'first_capture' || $paymentTermsRelativeTo == 'every_capture') {
+            $afterDelivery = true;
+        }
+
+        if (str_contains($name, 'eo')) {
+            $endOfMonth = true;
+        }
+
+        if ($endOfMonth && $afterDelivery) {
+            return __('%1 days end of month after delivery', $numberOfDays);
+        }
+
+        if ($endOfMonth) {
+            return __('%1 days end of month', $numberOfDays);
+        }
+
+        if ($afterDelivery) {
+            return __('%1 days after delivery', $numberOfDays);
+        }
+
+        return __('%1 days', $numberOfDays);
+    }
+}
