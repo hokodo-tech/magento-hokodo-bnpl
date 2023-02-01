9d0e692ef7f46bbe62c576bd6289351740340ce1
Doc Brown <ilia.sokolov@hokodo.co>
2023-02-01 13:25:18 +0400

    250-rebuild

--- CustomerData/HokodoCheckout.php
+++ CustomerData/HokodoCheckout.php
@@ -7,8 +7,10 @@ declare(strict_types=1);
 
 namespace Hokodo\BNPL\CustomerData;
 
+use Hokodo\BNPL\Api\Data\PaymentOffersInterface;
 use Hokodo\BNPL\Api\HokodoQuoteRepositoryInterface;
 use Hokodo\BNPL\Gateway\Service\Offer;
+use Hokodo\BNPL\Model\Webapi\Offer as WebapiOffer;
 use Magento\Checkout\Model\Session;
 use Magento\Customer\CustomerData\SectionSourceInterface;
 use Psr\Log\LoggerInterface;
@@ -18,6 +20,8 @@ class HokodoCheckout implements SectionSourceInterface
     public const USER_ID = 'userId';
     public const ORGANISATION_ID = 'organisationId';
     public const OFFER = 'offer';
+
+    public const OFFERED_PAYMENT_PLAN_STATUS = 'offered';
 
     /**
      * @var Session
@@ -40,20 +44,28 @@ class HokodoCheckout implements SectionSourceInterface
     private LoggerInterface $logger;
 
     /**
+     * @var WebapiOffer
+     */
+    private WebapiOffer $webapiOffer;
+
+    /**
      * @param Session                        $checkoutSession
      * @param HokodoQuoteRepositoryInterface $hokodoQuoteRepository
      * @param Offer                          $offerService
+     * @param WebapiOffer                    $webapiOffer
      * @param LoggerInterface                $logger
      */
     public function __construct(
         Session $checkoutSession,
         HokodoQuoteRepositoryInterface $hokodoQuoteRepository,
         Offer $offerService,
+        WebapiOffer $webapiOffer,
         LoggerInterface $logger
     ) {
         $this->checkoutSession = $checkoutSession;
         $this->hokodoQuoteRepository = $hokodoQuoteRepository;
         $this->offerService = $offerService;
+        $this->webapiOffer = $webapiOffer;
         $this->logger = $logger;
     }
 
@@ -80,9 +92,27 @@ class HokodoCheckout implements SectionSourceInterface
             }
         }
         $this->hokodoQuoteRepository->save($hokodoQuote);
-
+        $offer = $this->addIsEligibleFlag($offer);
         return [
             self::OFFER => $offer ? $offer->__toArray() : '',
         ];
+    }
+
+    /**
+     * Add is_eligible flag to offer.
+     *
+     * @param PaymentOffersInterface|string $offer
+     *
+     * @return PaymentOffersInterface|string|null
+     */
+    private function addIsEligibleFlag($offer = null)
+    {
+        if (!empty($offer)) {
+            $offer->setIsEligible(false);
+            if ($this->webapiOffer->isPaymentPlanHaveStatus($offer, self::OFFERED_PAYMENT_PLAN_STATUS)) {
+                $offer->setIsEligible(true);
+            }
+        }
+        return $offer;
     }
 }
